6cf72f439251980379d976f93d0aa6a14ebd7def
manoj1pradhan <pradhamj@mail.uc.edu>
2023-09-25 18:04:10 -0400

    v1.0

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class SalesTax {
+    public static void main(String[] args) {
+
+        double purchasePrice = 0;
+
+        double salesTax = purchasePrice * 0.05;
+
+        System.out.println("Purchase Price: $" + purchasePrice);
+        System.out.println("Sales Tax (5%): $" + salesTax);
+    }
+}
